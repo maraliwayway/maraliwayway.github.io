23a4b08063215d9b41a7385260bb02a9502c1ad8
Mara Liwayway David <davidmaraliwayway@gmail.com>
2025-02-09 14:54:55 -0800

    chore: configure next.js for github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/maraliwayway.github.io',
+};
 
 export default nextConfig;
