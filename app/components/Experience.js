11a9766164db3cceaf84a15cd02cae52ef8ac8f0
Mara Liwayway David <davidmaraliwayway@gmail.com>
2025-02-10 15:21:14 -0800

    feat: add work and volunteer experience headings for experience section

--- app/components/Experience.js
+++ app/components/Experience.js
@@ -1,7 +1,15 @@
 const Experience = () => {
     return (
-      <section id="experience" className="h-screen flex items-center justify-center">
-        <h1 className="text-5xl text-extralight">Experience</h1>
+      <section id="experience" className="h-screen flex items-center justify-center text-extralight">
+       
+        <div className="flex flex-col items-center gap-20">
+
+            <h1 className="text-5xl">Experience</h1>
+            <h2 className="text-3xl "> Work Experience</h2>
+            <h2 className="text-3xl "> Volunteer Experience</h2>
+        
+        </div>
+
       </section>
     );
   };
